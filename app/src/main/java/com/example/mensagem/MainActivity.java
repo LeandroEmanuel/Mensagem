a19bcaface2b9a5da622f4d40e0bb8ba1b5e3953
LeandroEmanuel <leandrog276@gmail.com>
2020-03-06 12:18:56 +0000

    Passar o texto da atividade para os recursos (strings)

--- app/src/main/java/com/example/mensagem/MainActivity.java
+++ app/src/main/java/com/example/mensagem/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.mensagem;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,7 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
-        //todo: implementar o interface
+
+    }
+
+    public void enviaMensagem(View view) {
+        //todo: enviar mensagem
     }
 }
